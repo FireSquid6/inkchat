922793bc15ab3e4ad4d1919b92fcb56ae8596230
firesquid6 <jdeiss06@gmail.com>
2024-06-11 21:23:46 -0500

    add tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,tsx,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
