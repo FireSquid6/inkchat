39c24fa157b08632f4096cdc907859c6d802d24b
firesquid6 <jdeiss06@gmail.com>
2024-06-11 21:20:26 -0500

    add capacitor

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'co.firesquid.inkchat',
+  appName: 'inkchat',
+  webDir: 'dist'
+};
+
+export default config;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,4 +3,7 @@ import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
+  server: {
+    port: 8080
+  }
 })
