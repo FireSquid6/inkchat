4298ca5a57bca0e31549a412caf76712350032c8
firesquid6 <jdeiss06@gmail.com>
2024-06-12 10:49:18 -0500

    add elysia to the server project

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import Elysia from "elysia";
+import { cors } from "@elysiajs/cors";
+
+
+const app = new Elysia()
+  .use(cors())
+  .get("/", () => {
+    return "Hello world!";
+  })
+
+
+
+export function startApp(loglevel: string = "info") {
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+  })
+}
+
+
+export type App = typeof app;
