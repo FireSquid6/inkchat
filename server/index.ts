cee62eaf770d2d668c81bf1b27bef18f38f1fde2
firesquid6 <jdeiss06@gmail.com>
2024-06-13 21:50:02 -0500

    add a proper logger

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,8 @@
-import Elysia from "elysia";
+import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
+import { getDb } from "./lib/db";
+import { getAuth } from "./lib/auth";
+import { makeLoggerManager } from "./lib/logger";
 
 
 export interface AppConfig {
@@ -10,10 +13,16 @@ export interface AppConfig {
 
 const app = new Elysia()
   .use(cors())
+
+  // these states are deliberately left empty. They are set by startApp
   .state("config", {
     port: 3000,
     storeDir: "./data"
   } as AppConfig)
+  .state("db", {} as ReturnType<typeof getDb>)
+  .state("auth", {} as ReturnType<typeof getAuth>)
+  .state("logger", {} as ReturnType<typeof makeLoggerManager>)
+
   .get("/", () => {
 
     return "Hello world!";
@@ -22,10 +31,21 @@ const app = new Elysia()
 
 
 export function startApp(config: AppConfig) {
-  app.state("config", config)
+  const db = getDb(config.storeDir)
+  const auth = getAuth(db)
+  const loggerManager = makeLoggerManager(config)
+
+  app.store.config = config;
+  app.store.db = db;
+  app.store.auth = auth;
+  app.store.logger = loggerManager;
+
   app.listen(config.port, () => {
     console.log(`Server is running on port ${config.port}`)
   })
+
+
+  return { db, auth, app }
 }
 
 
