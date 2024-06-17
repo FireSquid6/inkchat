1df7bda7d3bda84cf1117fd3b1e29e3842e7a908
firesquid6 <jdeiss06@gmail.com>
2024-06-17 12:03:57 -0500

    fix public links in the server

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,10 +3,6 @@
 # sets up the proper links and runs all if the install scripts
 cd $(dirname $0)
 
-cd ../protocol
-bun link
-bun install
-
 cd ../server
 bun link
 bun install
--- server/public.ts
+++ server/public.ts
@@ -1,7 +1,4 @@
 // the server package is depended on directly by the client for eden treaties and such
 // this file is where everything is exported directly
 export type { App } from "@/index"
-export * as ServerMessageProtocol from "@/protocol/server"
-export * as Protocol from "@/protocol"
-export * as ClientMessageProtocol from "@/protocol/client"
-
+export * as protocol from "@/protocol"
