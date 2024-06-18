b2ea811f21b86f86a6ee804310d7bd1e3eae9ca6
firesquid6 <jdeiss06@gmail.com>
2024-06-17 23:41:09 -0500

    move configs to their proper directories

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "index.html",
-    "src/**/*.{vue,js,ts,jsx,tsx}",
+    "client/**/*.{vue,js,ts,jsx,tsx,html}",
   ],
   theme: {
     extend: {},
