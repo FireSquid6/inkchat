90e42b0bbb5916febe8824299cd25debe24747be
firesquid6 <jdeiss06@gmail.com>
2024-06-19 22:18:06 -0500

    add swagger to elysia

--- server/api/index.ts
+++ server/api/index.ts
@@ -3,12 +3,14 @@ import { cors } from "@elysiajs/cors";
 import type { Kit } from "@/index";
 import type { User } from "lucia";
 import { Logestic } from "logestic";
+import { swagger } from "@elysiajs/swagger";
 
 import { protectedAuthApi, unprotectedAuthApi } from "@/api/auth"
 import { channelsApi } from "@/api/channels";
 import { usersApi } from "@/api/users";
 import { filesApi } from "@/api/files";
 import { connectionApi } from "@/api/connection"
+
 
 export const kitPlugin = (app: Elysia) => app
   // this is deliberately left empty. It is set whenever startApp() is called
@@ -59,6 +61,15 @@ export const kitPlugin = (app: Elysia) => app
 export const app = new Elysia()
   // up here is unprotected! No auth required
   .use(cors())
+  .use(swagger({
+    documentation: {
+      info: {
+        title: "Inkchat API",
+        description: "The Inkchat API",
+        version: "0.1.0"
+      }
+    }
+  }))
   .use(kitPlugin)
   .use(unprotectedAuthApi)
   .use(connectionApi)
