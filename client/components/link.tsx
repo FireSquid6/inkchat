d70af0bc8332a25f7e19dcb1131f70756760cade
firesquid6 <jdeiss06@gmail.com>
2024-06-29 20:14:02 -0500

    use nextlink

--- client/components/link.tsx
+++ client/components/link.tsx
@@ -8,14 +8,15 @@
 
 import * as Headless from '@headlessui/react'
 import React, { forwardRef } from 'react'
+import NextLink, { type LinkProps } from "next/link"
 
 export const Link = forwardRef(function Link(
-  props: { href: string } & React.ComponentPropsWithoutRef<'a'>,
-  ref: React.ForwardedRef<HTMLAnchorElement>
+  props: LinkProps & React.ComponentPropsWithoutRef<'a'>,
+  ref: React.ForwardedRef<HTMLAnchorElement>,
 ) {
   return (
     <Headless.DataInteractive>
-      <a {...props} ref={ref} />
+      <NextLink {...props} ref={ref} />
     </Headless.DataInteractive>
   )
 })
