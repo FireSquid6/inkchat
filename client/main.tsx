958ab77604c1094ce3cc36a99ad030e543ed484b
firesquid6 <jdeiss06@gmail.com>
2024-07-04 14:34:41 -0500

    reset to using daisyui

--- client/main.tsx
+++ client/main.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Layout from './layout'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { Root } from './pages/root'
+import Layout from "./layout";
+import { ConnectionProvider } from "./lib/context";
+import { ErrorPage } from './error'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/channels/:channelId",
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Layout>
-      <p className="text-3xl">Hello, world!</p>
-    </Layout>
+    <ConnectionProvider>
+      <Layout>
+        <RouterProvider router={router} />
+      </Layout>
+    </ConnectionProvider>
   </React.StrictMode>,
 )
