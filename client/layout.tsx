60c2dfee909fe270f7401f03dc83a1b1a09b9a9b
firesquid6 <jdeiss06@gmail.com>
2024-07-07 00:07:29 -0500

    make the quick links work better

--- client/layout.tsx
+++ client/layout.tsx
@@ -2,10 +2,11 @@ import { useCallback } from "react";
 import { RiMenuUnfold3Line, RiMenuFold3Line } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import { useState } from "react"
-import { FaHashtag, FaPlus } from "react-icons/fa6"
+import { FaHashtag, FaHouse, FaPlus, FaLinkSlash, FaCircleInfo, FaGear } from "react-icons/fa6"
 import { useConnection } from "./lib/context";
 import { getStoredSessions } from "./lib/auth";
 import { isSome } from "@/maybe";
+import { MdPerson } from "react-icons/md"
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
@@ -26,11 +27,74 @@ export default function Layout({ children }: Readonly<{ children: React.ReactNod
     </div>)
 }
 
-type ChannelProps = {
-  id: string
-  name: string
+function Sidebar() {
+  const connection = useConnection()
+  return (
+    <>
+      <div className="flex flex-row">
+        <IdentitySwitcher className="hidden lg:block" />
+        <label htmlFor="my-drawer-2" className="ml-auto m-2 btn btn-circle btn-outline drawer-button lg:hidden">
+          <RiMenuFold3Line size="1.5em" />
+        </label>
+      </div>
+      <QuickLinks />
+      <hr className="my-2" />
+      {connection.active ? (
+        <div className="overflow-y-auto h-full">
+          <ChannelList />
 
+        </div>) : (
+        <NotConnected />
+      )
+      }
+    </>
+  )
 }
+
+function QuickLinks() {
+  // TODO: make settings disappear if you're not an admin
+  // TODO: make the links work properly depending on the current connection
+  const connection = useConnection()
+  const quickLinks: QuickLinkProps[] = [
+    { icon: <FaHouse />, text: "Home", to: "/home" },
+  ]
+  
+  if (connection.active) {
+    quickLinks.concat([
+      { icon: <MdPerson />, text: "Profile", to: `/${connection.address}/profile` }, 
+      { icon: <FaCircleInfo />, text: "About", to: `/${connection.address}/about` },
+      { icon: <FaGear />, text: "Settings", to: `/${connection.address}/settings`},
+    ])
+  }
+
+  return (
+    <ul className="flex flex-col">
+      {
+        quickLinks.map((quickLink, i) => (
+          <QuickLink key={i} {...quickLink} />
+        ))
+      }
+    </ul>
+  )
+}
+
+type QuickLinkProps = {
+  icon: React.ReactNode
+  text: string
+  to: string
+}
+
+function QuickLink(props: QuickLinkProps) {
+  return (
+    <li className="w-full">
+      <Link to={props.to} className="flex flex-row items-center">
+        {props.icon}
+        <p className="font-bold">{props.text}</p>
+      </Link>
+    </li>
+  )
+}
+
 
 function ChannelList() {
   const channels: ChannelProps[] = [
@@ -50,13 +114,18 @@ function ChannelList() {
   )
 }
 
+type ChannelProps = {
+  id: string
+  name: string
+
+}
 function Channel(props: ChannelProps) {
   return (
     <li>
-      <a className="flex flex-row">
+      <Link className="flex flex-row" to={props.id}>
         <FaHashtag />
         <p>{props.name}</p>
-      </a>
+      </Link>
     </li>
   )
 }
@@ -74,33 +143,7 @@ function Topbar() {
   )
 }
 
-function Sidebar() {
-  const connection = useConnection()
-  return (
-    <>
-      <div className="flex flex-row">
-        <IdentitySwitcher className="hidden lg:block" />
-        <label htmlFor="my-drawer-2" className="ml-auto m-2 btn btn-circle btn-outline drawer-button lg:hidden">
-          <RiMenuFold3Line size="1.5em" />
-        </label>
-      </div>
 
-      {connection.active ? (
-        <div className="overflow-y-auto h-full">
-          <ChannelList />
-
-        </div>) : (
-        <div className="flex flex-col items-center justify-center h-full">
-          <p>Not connected right now!</p>
-        </div>
-      )
-      }
-
-
-    </>
-  )
-
-}
 
 
 function IdentitySwitcher(props: { className?: string }) {
@@ -152,11 +195,11 @@ function IdentitySwitcher(props: { className?: string }) {
 
 
   return (
-    <div className={`dropdown dropdown-bottom w-full mr-2 ${props.className}`}>
+    <div className={`dropdown dropdown-bottom w-full mr-2 lg:mr-0 ${props.className}`}>
       <div tabIndex={0} role="button" onClick={onMainButtonClicked} className="btn btn-primary flex w-full my-2">
         {
           selectedIdentity === null ? (
-            <p>Not logged in!</p>
+            <p>---------------</p>
           ) : (
             <Identity {...selectedIdentity} onClick={() => { }} />
           )
@@ -195,5 +238,15 @@ function Identity(props: IdentityProps) {
       <img src={props.image ?? "https://via.placeholder.com/150"} alt="server-icon" className="rounded-full w-8 h-8 mr-2" />
       <p>{props.address}</p>
     </Link>
+  )
+}
+
+
+function NotConnected() {
+  return (
+    <div className="flex flex-col items-center justify-center my-12 h-full">
+      <FaLinkSlash size="8em" className="my-8" />
+      <p className="text-xl">Not connected to any server</p>
+    </div>
   )
 }
