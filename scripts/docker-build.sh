1f8bff2dc89486f0d2f455b74b367b28a7a160c5
firesquid6 <jdeiss06@gmail.com>
2024-07-10 20:21:21 -0500

    add docker build script

---
+++ scripts/docker-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+VERSION=$1
+
+if [ -z "$VERSION" ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+docker build -t firesquid/inkchat:$VERSION .
+docker push firesquid/inkchat:$VERSION
