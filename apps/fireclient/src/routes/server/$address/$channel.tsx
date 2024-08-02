dbcc54fdbccfaa4985129f9a152f901983495c6a
firesquid6 <jdeiss06@gmail.com>
2024-08-01 22:40:15 -0500

    try to isolate the issues with the store

--- apps/fireclient/src/routes/server/$address/$channel.tsx
+++ apps/fireclient/src/routes/server/$address/$channel.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useRef, useState, useSyncExternalStore } from "react"
 import { messagesStore, connectionStore, updateMessages, usersStore } from '@/lib/store'
 import { createFileRoute } from '@tanstack/react-router'
 import { useStore } from '@tanstack/react-store'
@@ -11,32 +11,55 @@ export const Route = createFileRoute('/server/$address/$channel')({
 
 function ChannelComponent() {
   const channelId = Route.useParams().channel
-  const { data: connection } = useStore(connectionStore)
-
-  
-  if (connection !== null) {
-    updateMessages(connection, channelId)
-  }
-
-  const currentMessages = useStore(messagesStore)
-  let messages = currentMessages.get(channelId)
-
-  if (messages === undefined) {
-    messages = []
-  }
 
   return (
     <div className="h-full flex flex-col">
-      <div className="flex flex-col h-full overflow-y-auto m-4">
-        {messages.map((message, i) => (
-          <Message key={i} {...message} />
-        ))}
-      </div>
+      <Messages channelId={channelId} />
       <ChatInput channelId={channelId} />
     </div>
   )
 }
 
+function useMessagesStore() {
+  // ugly callback hell
+  return useSyncExternalStore((listener) => { 
+    console.log("the function you passed to syncExternalStore is being called")
+    const unsubscribe = messagesStore.subscribe(() => {
+      console.log("wrapper around the listener is being called")
+      listener()
+    })
+
+    return () => {
+      console.log("unsubscribe is being called")
+      unsubscribe()
+    }
+  }, () => messagesStore.state)
+}
+
+function Messages(props: { channelId: string }) {
+  const { data: connection } = useStore(connectionStore)
+  const dummyDiv = useRef<HTMLDivElement | null>(null)
+  const currentMessages = useMessagesStore()
+
+  let messages = currentMessages.get(props.channelId)
+  console.log(messages)
+
+  if (messages === undefined) {
+    if (connection !== null) {
+      updateMessages(connection, props.channelId)
+    }
+    messages = []
+  }
+
+  return (
+    <div className="flex flex-col h-full overflow-y-auto m-4">
+      {messages.map((message, i) => (
+        <Message key={i} {...message} />
+      ))}
+      <div ref={dummyDiv} />
+    </div>
+  )
+}
 
 function Message(message: MessageRow) {
   const users = useStore(usersStore)
@@ -75,7 +98,11 @@ function ChatInput(props: { channelId: string }) {
 
   return (
     <div className="flex flex-row m-2">
-      <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} className="input input-primary input-bordered w-full mr-2" />
+      <input type="text" value={message} onKeyUp={(e) => {
+        if (e.key === "Enter") {
+          onClick()
+        }
+      }} onChange={(e) => setMessage(e.target.value)} className="input input-primary input-bordered w-full mr-2" />
       <button disabled={connection === null} onClick={onClick} className="btn btn-primary">Send</button>
     </div>
   )
