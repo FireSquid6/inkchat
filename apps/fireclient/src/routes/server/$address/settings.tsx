07e11760af620bf871a6eaed7cc219f0cbf3a6cd
firesquid6 <jdeiss06@gmail.com>
2024-08-03 11:28:59 -0500

    fix the settings file

--- apps/fireclient/src/routes/server/$address/settings.tsx
+++ apps/fireclient/src/routes/server/$address/settings.tsx
@@ -1,5 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useStore } from '@tanstack/react-store'
+import { connectionStore } from "@/lib/store"
+import useSWR from "swr"
 
 export const Route = createFileRoute('/server/$address/settings')({
-  component: () => <div>Hello /server/$address/settings!</div>
+  component: () => Settings
 })
+
+
+function Settings() {
+  // TODO - make this a `useUser` hook
+  const connection = useStore(connectionStore)
+  const { data, error, isLoading } = useSWR("current-user", () => {
+    if (connection === null) {
+      throw new Error("No connection")
+    }
+  })
+
+
+  return (
+    <p></p>
+  )
+}
