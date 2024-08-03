24ab13c4a1b541f7e8b5e539068ac64d924d9bd4
Jonathan Deiss <jdeiss@gmail.com>
2024-08-02 21:36:24 -0500

    remove unused store package

--- packages/store/react.ts
+++
@@ -1,6 +0,0 @@
-import { useSyncExternalStore } from "react"
-import { ObserverStore } from "./"
-
-export function useStore<T>(store: ObserverStore<T>) {
-  return useSyncExternalStore(store.observe, store.snapshot)
-}
