4265b89e1a1d763106e82f93fd55fbb2015ca726
firesquid6 <jdeiss06@gmail.com>
2024-08-06 12:32:05 -0500

    add a prttier config

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+  trailingComma: "none",
+  tabWidth: 2,
+  semi: false,
+  printWidth: 80,
+  singleQuote: false,
+  quoteProps: "consistent",
+  bracketSpacing: true,
+  bracketSameLine: false,
+}
