c6146e3fd9c7e2ed2e07ebc24ee9f190d12c6d4c
Jonathan Deiss <jdeiss@gmail.com>
2024-08-10 00:06:57 -0500

    restructure app to make testing the frontend easier

---
+++ apps/fireclient/src/app.tsx
@@ -0,0 +1,16 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router"
+import { routeTree } from "./routeTree.gen"
+
+const router = createRouter({ routeTree })
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router
+  }
+}
+export function App() {
+  return (
+    <RouterProvider router={router} />
+  )
+
+}
--- apps/fireclient/src/main.tsx
+++ apps/fireclient/src/main.tsx
@@ -1,24 +1,14 @@
 import "./index.css"
 import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
-import { RouterProvider, createRouter } from "@tanstack/react-router"
-
-import { routeTree } from "./routeTree.gen"
-
-const router = createRouter({ routeTree })
-
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router
-  }
-}
+import { App } from "./app"
 
 const rootElement = document.getElementById("root")!
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <App />
     </StrictMode>
   )
 }
