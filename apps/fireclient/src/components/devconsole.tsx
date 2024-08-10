cb498050546e604e74aa63d52054d2e3472339af
Jonathan Deiss <jdeiss@gmail.com>
2024-08-10 00:01:56 -0500

    use an icon for the debug button

--- apps/fireclient/src/components/devconsole.tsx
+++ apps/fireclient/src/components/devconsole.tsx
@@ -1,4 +1,5 @@
 import { printDebug } from "@/lib/debug"
+import { FaTerminal } from "react-icons/fa6"
 
 export function DevConsole() {
   // we don't want this to show up in production
@@ -8,10 +9,10 @@ export function DevConsole() {
 
   return (
     <button
-      className="fixed top-0 right-0 m-2 text-sm btn btn-secondary"
+      className="fixed top-0 right-0 m-2 text-sm btn btn-secondary rounded-full"
       onClick={printDebug}
     >
-      Application State
+      <FaTerminal />
     </button>
   )
 }
