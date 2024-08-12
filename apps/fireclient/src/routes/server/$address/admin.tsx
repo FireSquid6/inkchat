ecc112cd956f76da8bcef0764f7ac6b188151239
Jonathan Deiss <jdeiss@gmail.com>
2024-08-11 23:28:05 -0500

    add a better system for creating channels

--- apps/fireclient/src/routes/server/$address/admin.tsx
+++ apps/fireclient/src/routes/server/$address/admin.tsx
@@ -3,7 +3,7 @@ import { createFileRoute } from "@tanstack/react-router"
 import { useStore } from "@tanstack/react-store"
 import { ChannelRow } from "api"
 import { useCallback, useRef, useState } from "react"
-import { FaCopy } from "react-icons/fa"
+import { FaCopy, FaTrash, FaPen } from "react-icons/fa"
 
 export const Route = createFileRoute("/server/$address/admin")({
   component: () => <Admin />
@@ -23,6 +23,7 @@ function Admin() {
   return (
     <div className="m-4">
       <JoincodeGenerator />
+      <ChannelCreator />
     </div>
   )
 }
@@ -90,32 +91,86 @@ function JoincodeGenerator() {
 function ChannelCreator() {
   const channels = useStore(channelStore)
   const [connection] = useStore(connectionStore)
+  const [focusedChannel, setFocusedChannel] = useState<ChannelRow | null>(null)
+  const deleteModalRef = useRef<HTMLDialogElement>(null)
+  const editModalRef = useRef<HTMLDialogElement>(null)
+
+  if (connection === null) {
+    return <p>No connection</p>
+  }
 
   const onDelete = useCallback((channel: ChannelRow) => {
-    connection?.sendMessage
+    setFocusedChannel(channel)
+    deleteModalRef.current?.showModal()
+  }, [connection, setFocusedChannel])
+  const onEdit = useCallback((channel: ChannelRow) => {
+    setFocusedChannel(channel)
+    editModalRef.current?.showModal()
+  }, [connection, setFocusedChannel])
 
-  }, [connection])
-
+  const deleteChannel = useCallback(() => {
+    deleteModalRef.current?.close()
+    if (focusedChannel === null) {
+      console.error("No channel to delete")
+      return
+    }
+    connection.deleteChannel(focusedChannel.id)
+  }, [connection, focusedChannel])
 
   return (
-    <div>
-
-    </div>
+    <>
+      <h1 className="my-16 m-4 text-4xl">Modify Channels</h1>
+      <div className="flex flex-col">
+        {
+          channels.map((channel) => (
+            <Channel key={channel.id} channel={channel} onEdit={onEdit} onDelete={onDelete} />
+          ))
+        }
+      </div>
+      <dialog id="delete-modal" ref={deleteModalRef} className="modal">
+        <div className="modal-box">
+          <h2 className="font-bold text-lg">
+            Are you sure you want to delete the channel "{focusedChannel?.name}"
+          </h2>
+          <div className="flex flex-row modal-action">
+            <button className="btn mr-4" onClick={() => deleteModalRef.current?.close()}>
+              No
+            </button>
+            <button className="btn btn-error" onClick={deleteChannel}>
+              Yes
+            </button>
+          </div>
+        </div>
+      </dialog>
+      <dialog id="edit-modal" className="modal" ref={editModalRef}>
+        <div className="modal-box">
+          <h2 className="font-bold text-lg">Under construction!</h2>
+          <div className="modal-action">
+            <button className="btn">Ok</button>
+          </div>
+        </div>
+      </dialog>
+    </>
   )
 }
 
 type ChannelProps = {
   channel: ChannelRow
   onDelete?: (channel: ChannelRow) => void
+  onEdit?: (channel: ChannelRow) => void
 }
 function Channel(props: ChannelProps) {
-  const [editing, setEditing] = useState<boolean>(false)
-  const [channelName, setChannelName] = useState<string>(props.channel.name)
-
+  // TODO - shorten channel name
+  // TODO - drag and drop channels to sort position
   return (
-    <div className="flex flex-row">
-      <input className="" />
-
+    <div className="flex flex-row m-2 p-2 rounded-lg bg-base-200 w-full">
+      <p className="text-lg font-bold w-full my-auto ml-4">{props.channel.name}</p>
+      <button className="btn btn-info mx-1" onClick={() => props.onEdit?.(props.channel)}>
+        <FaPen />
+      </button>
+      <button className="btn btn-error mx-1" onClick={() => props.onDelete?.(props.channel)}>
+        <FaTrash />
+      </button>
     </div>
   )
 
