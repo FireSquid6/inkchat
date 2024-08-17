3628dc506b7ddbafad3aaaf3cd57582b2373f382
firesquid6 <jdeiss06@gmail.com>
2024-08-16 22:06:41 -0500

    remove useCallback from imports

--- apps/fireclient/src/components/auth-form.tsx
+++ apps/fireclient/src/components/auth-form.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react"
+import { useState } from "react"
 import { type Failable, Err } from "maybe"
 import { sdk } from "api"
 import { storeSession } from "@/lib/auth"
